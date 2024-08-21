c54ee94a5491da85b4f480c8312c430d9534298e
Nhat Anh <ltnanh21@gmail.com>
2024-08-21 15:44:52 +0700

    Configure ModelMapper to not map null or empty properties

--- src/main/java/com/anhn/hotel_reservation_system/config/ModelMapperConfiguration.java
+++ src/main/java/com/anhn/hotel_reservation_system/config/ModelMapperConfiguration.java
@@ -1,6 +1,9 @@
 package com.anhn.hotel_reservation_system.config;
 
+import org.modelmapper.Condition;
+import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +12,16 @@ public class ModelMapperConfiguration {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+
+        //Not null or empty in source
+        Condition<?, ?> notNullOrEmpty = new Condition<Object, Object>() {
+            @Override
+            public boolean applies(MappingContext<Object, Object> mappingContext) {
+                return mappingContext.getSource() == null || (mappingContext.getSource() instanceof String && ((String) mappingContext.getSource()).isEmpty());
+            }
+        };
+        modelMapper.getConfiguration().setPropertyCondition(notNullOrEmpty);
+        return modelMapper;
     }
 }

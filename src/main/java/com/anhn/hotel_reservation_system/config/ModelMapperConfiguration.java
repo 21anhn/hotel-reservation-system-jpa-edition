25037fd6086e11e85a9ae878f17613c9a6a8c4e1
Nhat Anh <ltnanh21@gmail.com>
2024-08-23 08:44:17 +0700

    Set condition to map data

--- src/main/java/com/anhn/hotel_reservation_system/config/ModelMapperConfiguration.java
+++ src/main/java/com/anhn/hotel_reservation_system/config/ModelMapperConfiguration.java
@@ -1,7 +1,6 @@
 package com.anhn.hotel_reservation_system.config;
 
 import org.modelmapper.Condition;
-import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
@@ -18,7 +17,7 @@ public class ModelMapperConfiguration {
         Condition<?, ?> notNullOrEmpty = new Condition<Object, Object>() {
             @Override
             public boolean applies(MappingContext<Object, Object> mappingContext) {
-                return mappingContext.getSource() == null || (mappingContext.getSource() instanceof String && ((String) mappingContext.getSource()).isEmpty());
+                return mappingContext.getSource() != null && (!(mappingContext.getSource() instanceof String) || !((String) mappingContext.getSource()).isEmpty());
             }
         };
         modelMapper.getConfiguration().setPropertyCondition(notNullOrEmpty);

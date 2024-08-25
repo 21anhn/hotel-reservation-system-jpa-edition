44fa02eb9db5cf06c93d7a87c5a94894114bdace
Nhat Anh <ltnanh21@gmail.com>
2024-08-25 14:54:45 +0700

    Check null before convert

--- src/main/java/com/anhn/hotel_reservation_system/utils/Utils.java
+++ src/main/java/com/anhn/hotel_reservation_system/utils/Utils.java
@@ -6,11 +6,15 @@ import java.time.format.DateTimeFormatter;
 public class Utils {
 
     public static LocalDate convertStringToLocalDate(String date, String format) {
+        if(date == null || date.isEmpty() || format == null || format.isEmpty())
+            return null;
         DateTimeFormatter.ofPattern(format);
         return LocalDate.parse(date, DateTimeFormatter.ofPattern(format));
     }
 
     public static String convertLocalDateToString(LocalDate date, String format) {
+        if(date == null)
+            return null;
         DateTimeFormatter.ofPattern(format);
         return date.format(DateTimeFormatter.ofPattern(format));
     }
